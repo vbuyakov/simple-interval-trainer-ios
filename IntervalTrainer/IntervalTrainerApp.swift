a632044bfcb561ddee578cfba1dc7a388ceeae7a
Viktor Buiakov <viktorbuyakov@cloud.upwork.com>
2025-04-14 19:54:18 +0200

    Initial Commit

---
+++ IntervalTrainer/IntervalTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalTrainerApp.swift
+//  IntervalTrainer
+//
+//  Created by Viktor Buiakov on 14/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct IntervalTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
